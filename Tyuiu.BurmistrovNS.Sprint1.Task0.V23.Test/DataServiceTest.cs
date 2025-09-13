09a7c83d85d3d8a918f113d559c1ed90c545327e
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-09-13 22:13:57 +0500

    Add project files.

---
+++ Tyuiu.BurmistrovNS.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BurmistrovNS.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
