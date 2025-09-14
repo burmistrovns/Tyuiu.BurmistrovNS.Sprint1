2659ca4667161388a5660fbdb04625e9d6473596
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-09-14 14:39:21 +0500

    Task3

---
+++ Tyuiu.BurmistrovNS.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BurmistrovNS.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 100;
+            double wait = 106.67999999999999;
+            var res = ds.VerstsToKilometers(x);
+            Assert.AreEqual(wait, res);
+        }
+        
+
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BurmistrovNS.Sprint1.Task3.V7.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint1.Task3.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.WriteLine("****************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                             *");
+            Console.WriteLine("****************************************************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите количество километров X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                   *");
+            Console.WriteLine("****************************************************************************************************************");
+
+
+            Console.WriteLine("Количество вёрст = " + Math.Round(ds.VerstsToKilometers(x),3));
+
+            Console.ReadLine();
+
+
+
+
+        }
+
+
+    }
+}
